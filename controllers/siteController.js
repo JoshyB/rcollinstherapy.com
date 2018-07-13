6dbedabe32acaea80f902383de44562df635ae81
seejoshcode@gmail.com <seejoshcode@gmail.com>
2018-07-13 14:44:42 -0400

    Mostly finished version of rebecca's website

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,44 +1,56 @@
 const mail = require("../handlers/mail");
+const quote = require("../quotes");
+
+function getQuote() {
+  //generate random integer between 0 and the length of the quotes array
+  const randomNum = Math.floor(Math.random() * quote.quotes.length)
+  return quote.quotes[randomNum]
+};
 
 exports.homePage = (req, res) => {
   res.render("home", {
     title: "Rebecca Collins, LISW",
+    pageDescription: "This is the landing page for RCollinstherapy.com. Rebecca Collins, LISW is a sex therapist located in Columbus, Ohio",
     bg__imageClass: "navigation__bg1"
   });
-  req.flash("hello there, this is a message");
 };
 
 exports.aboutPage = (req, res) => {
   res.render("about", {
     title: "About Rebecca",
+    pageDescription: "A short biography on Rebecca Collins",
     bg__imageClass: "navigation__bg2"
   });
 };
 
 exports.locationPage = (req, res) => {
   res.render("location", {
-    titleIcon: "./images/icons/map.svg",
-    bg__imageClass: "navigation__bg3"
+    bg__imageClass: "navigation__bg3",
+    pageDescription: "Location details, office hours, and contact information for Rebecca Collins, LISW",
+    quote: getQuote()
   });
 };
 
 exports.contactMe = (req, res) => {
   res.render("contactme", {
-    titleIcon: "./images/icons/message.svg",
+    quote: getQuote(),
+    pageDescription: "Rebecca Collins, LISW contact information, and contact form",
     bg__imageClass: "navigation__bg4"
   });
 };
 
 exports.formDownloads = (req, res) => {
   res.render("forms", {
-    titleIcon: "./images/icons/forms.svg",
+    quote: getQuote(),
+    pageDescription: "Rebecca Collins, LISW intake forms and policies",
     bg__imageClass: "navigation__bg5"
   });
 };
 
 exports.services = (req, res) => {
   res.render("services", {
-    title: "services",
+    quote: getQuote(),
+    pageDescription: "Services that Rebecca Collins, LISW offers",
     bg__imageClass: "navigation__bg6"
   });
 };
---
+++ quotes.js
@@ -0,0 +1,30 @@
+exports.quotes = [
+  {
+    quote: "Had I not created my whole world. I would certainly have died in other people’s ",
+    author: "Anais Nin"
+  },
+  {
+    quote: "Only the united beat of sex and the heart can create ecstasy.",
+    author: "Anais Nin"
+  },
+  {
+    quote: "I must be a mermaid. I have no fear of depths, and a great fear of shallow living",
+    author: "Anais Nin"
+  },
+  {
+    quote: "The therapist’s job is not to be an exterminator to take things away. The job is to give the problem back to the client in a way that shows how much they need it, show its value.",
+    author: "Thomas Moore"
+  },
+  {
+    quote: "Respect yourself enough to walk away from anything that  no longer serves you, grows you, or makes you happy.",
+    author: "Robert Tew"
+  },
+  {
+    quote: "There's more room in a broken heart",
+    author: "Carly Simon"
+  },
+  {
+    quote: "Pain is inevitable, Suffering is optional",
+    author: "Haruki Murakami"
+  },
+]
