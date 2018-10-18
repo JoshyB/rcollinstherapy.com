c6dfc3a528f834018faf0775d3e1cd8e0858f1c6
seejoshcode@gmail.com <seejoshcode@gmail.com>
2018-10-18 10:08:50 -0400

    wrapped the carousel and map elements in a try/catch to handle errors with those elements were not present on the currently viewed page

--- public/js/main.js
+++ public/js/main.js
@@ -10,36 +10,46 @@ openBtn.addEventListener("click", () => {
   mobileNavMenu.style.width = "300px";
 });
 
-// controls for the carousel gallery on the location page
-let slideIndex = 0;
+// try and catch as the carousel is on located on the locations page
+try {
+  // controls for the carousel gallery on the location page
+  let slideIndex = 0;
 
-carousel();
+  carousel();
 
-function carousel() {
-  const galleryImages = document.querySelectorAll(".main-img img");
-  for (var i = 0; i < galleryImages.length; i++) {
-    galleryImages[i].style.display = "none";
+  function carousel() {
+    const galleryImages = document.querySelectorAll(".main-img img");
+    for (var i = 0; i < galleryImages.length; i++) {
+      galleryImages[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > galleryImages.length) {
+      slideIndex = 1;
+    }
+    galleryImages[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 4000);
   }
-  slideIndex++;
-  if (slideIndex > galleryImages.length) {
-    slideIndex = 1;
-  }
-  galleryImages[slideIndex - 1].style.display = "block";
-  setTimeout(carousel, 4000);
+} catch (error) {
+  //do nothing
 }
 
-// initialize google maps for the locations page;
-const mapView = document.getElementById("map");
+// try and catch since the map element only exists on one page.
+try {
+  // initialize google maps for the locations page;
+  const mapView = document.getElementById("map");
 
-function makeMap() {
-  if (!mapView) return; //if there is no map div on the page...move along
-  const rebeccasOffice = { lat: 40.044, lng: -83.019 };
-  const map = new google.maps.Map(mapView, {
-    zoom: 16,
-    center: rebeccasOffice
-  });
-  const marker = new google.maps.Marker({
-    position: rebeccasOffice,
-    map: map
-  });
+  function makeMap() {
+    if (!mapView) return; //if there is no map div on the page...move along
+    const rebeccasOffice = { lat: 40.044, lng: -83.019 };
+    const map = new google.maps.Map(mapView, {
+      zoom: 16,
+      center: rebeccasOffice
+    });
+    const marker = new google.maps.Marker({
+      position: rebeccasOffice,
+      map: map
+    });
+  }
+} catch (error) {
+  // do nothing
 }
